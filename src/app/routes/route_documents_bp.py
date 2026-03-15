bb15eb293cd5c0989fdde8052b31d77fbf1435fc
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-03-15 09:36:14 +0500

    feat(documents): fill document template with input data

--- src/app/routes/route_documents_bp.py
+++ src/app/routes/route_documents_bp.py
@@ -6,7 +6,7 @@ documents_bp = Blueprint("documents", __name__, url_prefix="/documents")
 @documents_bp.get("/new")
 def new_document_form() -> str:
     return render_template(
-        "docoments/new.html",
+        "documents/new.html",
         page_title="Новый документ",
     )
 
