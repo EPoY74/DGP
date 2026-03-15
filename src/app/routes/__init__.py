4536568e24167f40abb19b21ea99fba4e7f30b89
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-03-15 09:31:27 +0500

    chore(routes): Select correct language mode

--- src/app/routes/__init__.py
+++ src/app/routes/__init__.py
@@ -1,5 +1,7 @@
 from app.routes.route_documents_bp import documents_bp
-
 from app.routes.route_main_bp import main_bp
 
-__all__ = ["main_bp", "documents_bp"]
+__all__ = [
+    "main_bp",
+    "documents_bp",
+]
