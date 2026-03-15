06c1ea9370e63afe19447e322b31b00e05025c1b
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-03-15 09:32:23 +0500

    feat(documents): add documents blueprint

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
-from app.routes.route_main_bp import main_bp
+from app.routes import (
+    documents_bp,
+    main_bp,
+)
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
 
     app.register_blueprint(main_bp)
+    app.register_blueprint(documents_bp)
 
     return app
